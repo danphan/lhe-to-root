8ff6d3a8bf5071e7769cd8f030f4760f5101161a
Dan Phan <dan@umail.ucsb.edu>
2014-12-28 02:10:31 -0800

    Comments are added. The names of variables are updated, and a memory leak is fixed. The loop over weights is removed.

--- looper.C
+++ looper.C
@@ -2,10 +2,9 @@
 //Author: Dan Phan (dan@umail.ucsb.edu)
 //Date: December 2014
 //Description/Instructions: Go to line 22, change name of file to your LHE file
-//Change tree name to your root tree name
-//Change name of output file
+//Change tree name to your desired root tree name
+//Change name of output file to your desired output filename
 //----------------------------------------------------------------------------
-#pragma GCC diagnostic ignored "-Wwrite-strings"
 
 #include <iostream>
 #include <fstream>
@@ -20,19 +19,33 @@
 using namespace std;
 
 //Parameters
-char* filename  = "lhe_file_toy";
+char* filename  = "/home/users/danphan/lhe-to-root/lhe_file_toy";
 char* outputName = "LHE_File";
 char* treeName = "LHE_Tree";
 
-
 typedef ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > LorentzVector;
+
+//fill tree for only maxEvents number of events
+int numEvents = 0; //initializing counter of events
+int maxEvents = 10e9;  //if you want to fill all events, make maxEvents huge
+
+//function to take store reweights
+float store_reweights(string line) {
+
+  float wgt;
+  int begin = line.find(">"); //beginning of number
+  int end = line.find("<",begin);  //end of number
+  float wgt= ::atof(line.substr(begin+1,end-(begin+1)).c_str()); //defines wgt as substring of line holding reweight
+                                                                 //between begin and end
+  return wgt;
+}
 
 int looper(){
 
 
   //Declare TTree and TFile
   TFile *file = new TFile(Form("%s.root", outputName), "RECREATE");
-  TTree *tree = new TTree("tree", Form("%s",treeName));
+  TTree *tree = new TTree("tree_qqww_mm", Form("%s",treeName));
   
   //Declare variables that will be stored in tree
   vector <int> pdgID;
@@ -43,16 +56,19 @@ int looper(){
   vector <int> colour_2;
   vector <LorentzVector> four_momentum;  //encodes 4-momentum and position
   vector <LorentzVector> four_position;
-  vector <float> row1; //row underneath <event>
-  int nParticles;      //entries in row1
+
+  int nParticles;      //variables in row1, first row beneath "<event>"
   int process_number;
-  float reweight;
+  float weight;
   float energy_scale;
   float QED_coupling;
   float QCD_coupling;
-  vector <float> weight;  
+
+  vector <float> reweight; //variable not in main block of code
   
   //Match up variable with branch
+  
+  //variables to be filled in second_line_below loop
   tree->Branch("pdgID", &pdgID); 
   tree->Branch("status", &status);
   tree->Branch("mother_1", &mother_1);
@@ -61,7 +77,11 @@ int looper(){
   tree->Branch("colour_2", &colour_2);
   tree->Branch("four_momentum", &four_momentum);
   tree->Branch("four_position", &four_position);
+  
+  //filled separate from first_below_event and second_line_below loops
   tree->Branch("weight",&weight); 
+
+  //filled in first_below_event loop
   tree->Branch("nParticles",&nParticles);
   tree->Branch("process_number",&process_number);
   tree->Branch("reweight",&reweight);
@@ -69,30 +89,35 @@ int looper(){
   tree->Branch("QED_coupling",&QED_coupling); 
   tree->Branch("QCD_coupling",&QCD_coupling); 
  
-  string line;
-  bool first = false; //whether on line right below event
-  bool second = false; //between <event> and </event>, turns on one line after "first", both turn off after filling tree
-  bool is_weight = false;
+  string line; //line of LHE file, to be looped over in main part of looper()
 
-  //declare temp vector used to fill other variables
-  vector<float> row;
-  
+  bool first_line_below = false; //whether on line right below event
+  bool second_line_below = false; //between <event> and </event>, is turned to true one line after "first_line_below", both turn off after filling tree
+  bool is_reweight = false;  //notes whether data contains reweight data
+
+  //declare vectors used to fill other variables
+  vector<float> row;  //row used to fill variables in second_line_below loop
+  vector<float> row1; //row used to fill variables in first_line_below part of looper() 
+
   //Opening data file
   fstream myfile (Form("%s.lhe",filename), ios_base::in);
 
-  while (getline(myfile,line)){          
+  while (getline(myfile,line)){  //loops through file and fills line(the variable) with that line of the file
+ 
+    if (numEvents > maxEvents) break;  //fills tree only for maxEvents number of events
 
-    //if on 2nd line below event, fill variables(row1 and weight entries filled in other loops)
-    if (second == true) {   
+    //if on 2nd line below "<event>", fill variables(row1 and reweight entries filled in other loops)
+    if (second_line_below == true) {   
       do {  
-        if (line.find('#') != string::npos) break;  //if line contains "stop", stop loop
+        if (line.find('#') != string::npos) break;  //if line contains '#', line does not contain data anymore, break out of loop
       
-        //iss has entire string.  Now read first part of string (pdgID) into "val" 
-        istringstream iss;   //opening string stream, above getline loops past first line below "yes"
+        //iss contains line
+        istringstream iss;   //opening string stream
         iss.str(line);      //copying line into stream
-        float val;
+
+        float val; 
          
-     	while (iss >> val) row.push_back(val);  //fill row with numbers in line
+     	while (iss >> val) row.push_back(val);  //fill row with data from line
 
         //Now split row into vectors for each variable
         pdgID.push_back(row[0]); 
@@ -111,65 +136,69 @@ int looper(){
         four_position_temp.SetXYZT(row[10],row[11],row[12],0);  //no time information in file, we have set t=0
         four_position.push_back(four_position_temp);
 
-        row.clear();
+        row.clear();  //clears row so we can fill it again with next line of data
 
-      } while (getline(myfile,line));
-      first = false;
-      second = false;  //after array prints, turn first, second off again
+     } while (getline(myfile,line));
+
+      first_line_below = false;
+      second_line_below = false;  // turn first_line_below, second_line_below off so we know we're done with those loops
     }
  
     //Line right below <event>(row1 variables)
-    if (first == true) {
-      second = true; //sets up next loop
+    if (first_line_below == true) {
+      second_line_below = true; //sets up loop over second_line_below variables
+
       istringstream iss;
       iss.str(line);
+
       float val1;
 
       while (iss >> val1) row1.push_back(val1);  //fill row1 with numbers in line
 
       nParticles = row1[0];
       process_number = row1[1];
-      reweight = row1[2];
+      weight = row1[2];
       energy_scale = row1[3];
       QED_coupling = row1[4];
       QCD_coupling = row1[5];
+
+      row1.clear(); //clears row1 for next event 
       
     }
     
-    //Store weights 
-    if (line.find("wgt id") != string::npos) { //if line contains wgt id, start filling weight
-      do {
-        if (!(line.find("wgt id") != string::npos)) { //if line doesn't contain wgt id, stop filling and break out of loop
-          break;
-        }
-        int begin = line.find(">");
-        int end = line.find("<",begin);
-        const char* str_val2 = line.substr(begin+1,end-(begin+1)).c_str(); //takes number out of line
-        float val2 = atof(str_val2); //turns number from string to float 
-        weight.push_back(val2);
-      } while (getline(myfile,line));
-      is_weight = true; 
+    //Store reweights 
+    if (line.find("wgt id") != string::npos) { //if line contains wgt id, start filling reweight
+
+      //wgt is substring of line that contains the reweight information
+      float wgt = store_reweights(line);
+      reweight.push_back(wgt); //fill reweight vector
+
+      is_reweight = true; //note that events contain reweight information
     }
 
-    //Check every line after </event> 
-    if ((line.find("<event>") != string::npos)) {  //if line contains  "<event>", we turn first to true(begins filling row1 variables)
+    //Check every line after </event> to see if we've reached the next event 
+    if ((line.find("<event>") != string::npos)) {  //if line contains  "<event>", we turn first_line_below to true(begins filling row1 variables)
 
-      //if file doesn't contain weight info, fill weight with nonsense 
-      if (is_weight == false) weight.push_back(-5);
+      //if file doesn't contain reweight info, fill reweight with nonsense 
+      if (is_reweight == false) reweight.push_back(-5);
 
-      if (pdgID.size() != 0) tree->Fill();   //if vectors containing data are nonempty, fill tree(pdgID picked arbitrarily)
-      pdgID.clear();
+      if (pdgID.size() != 0){
+        tree->Fill();   //only fill tree if vectors are nonempty(pdgID picked arbitrarily)
+        numEvents++;
+      }
+  
+      //clear vectors so we can fill them again for next event
+      pdgID.clear();  
       status.clear();
       mother_1.clear();
       mother_2.clear();
       colour_1.clear();
       colour_2.clear();
-      weight.clear();
-      row1.clear();
+      reweight.clear();
       four_momentum.clear();
       four_position.clear();
 
-      first = true; //begins process to move onto next event
+      first_line_below = true; //begins process to move onto next event
     }
     
   }
